d3e1429cb5c85ac2ba34cd0b00131dab87a09211
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-04-11 15:58:26 +0200

    Update main.jsx

--- react-libraries/src/main.jsx
+++ react-libraries/src/main.jsx
@@ -4,7 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
-import Contact from ".routes/contact";
+import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
   {
