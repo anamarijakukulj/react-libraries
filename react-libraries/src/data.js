da77522af482f3691e44911bb365da3a35c69f90
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-04-13 10:13:11 +0200

    Change path

--- react-libraries/src/data.js
+++ react-libraries/src/data.js
@@ -1,13 +1,13 @@
-import tour1 from "./images/tour-1.jpeg";
-import tour2 from "./images/tour-2.jpeg";
-import tour3 from "./images/tour-3.jpeg";
-import tour4 from "./images/tour-4.jpeg";
+import tour1 from "./assets/tour-1.jpeg";
+import tour2 from "./assets/tour-2.jpeg";
+import tour3 from "./assets/tour-3.jpeg";
+import tour4 from "./assets/tour-4.jpeg";
 
 export const pageLinks = [
-  { id: 1, href: "#home", text: "home" },
-  { id: 2, href: "#about", text: "about" },
-  { id: 3, href: "#services", text: "services" },
-  { id: 4, href: "#tours", text: "tours" },
+  { id: 1, href: "/", text: "home" },
+  { id: 2, href: "about/", text: "about" },
+  { id: 3, href: "tours/", text: "tours" },
+  { id: 4, href: "contact/", text: "contact" },
 ];
 
 export const socialLinks = [
