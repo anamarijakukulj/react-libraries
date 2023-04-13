4f6b9b680ba78a2cf15b22634bef9bafd1e7f050
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-04-13 09:43:06 +0200

    Update home.jsx

--- react-libraries/src/routes/home.jsx
+++ react-libraries/src/routes/home.jsx
@@ -1,46 +1,12 @@
 import { Link } from 'react-router-dom';
+import Hero from '../components/Hero';
+import AboutUs from '../components/About-us';
 
 export default function Home() {
   return (
     <>
-        <section className="hero" id="home">
-            <div className="hero-banner">
-                <h1>continue exploring</h1>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-                  explicabo debitis est autem dicta.
-                </p>
-                <Link to={'tours/'} className="btn hero-btn">
-                  explore tours
-                </Link>
-            </div>
-        </section>
-
-        <section className="section" id="about">
-            <Title title="about" subTitle="us" />
-
-            <div className="section-center about-center">
-              <div className="about-img">
-                <img src={imageAbout} className="about-photo" alt="awesome beach" />
-              </div>
-              <article className="about-info">
-                <h3>explore the difference</h3>
-                <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
-                  quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
-                  unde dolor?
-                </p>
-                <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
-                  quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
-                  unde dolor?
-                </p>
-                <a href="#" className="btn">
-                  read more
-                </a>
-              </article>
-            </div>
-        </section>
+       <Hero />
+       <AboutUs />
     </>
 )}
 
