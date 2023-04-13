c86bcb0dd3f4d9dc48c28feb4e12fc0a154aafab
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-04-13 10:38:57 +0200

    Update home.jsx

--- react-libraries/src/routes/home.jsx
+++ react-libraries/src/routes/home.jsx
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom';
 import Hero from '../components/Hero';
 import AboutUs from '../components/About-us';
+import Services from '../components/Services';
+import Tour from '../components/Tour';
 
 export default function Home() {
   return (
     <>
        <Hero />
        <AboutUs />
+       <Services />
+       <Tour />
     </>
 )}
 
