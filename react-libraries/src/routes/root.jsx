0ccfda948b3b1d86128dc4cf6241dd91d50d4eef
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-04-13 10:15:42 +0200

    Add Header

--- react-libraries/src/routes/root.jsx
+++ react-libraries/src/routes/root.jsx
@@ -1,31 +1,9 @@
-import { Outlet, Link } from "react-router-dom";
+import Header from "../components/Header";
 
 export default function Root() {
   return (
     <>
-    <header className="navbar">
-      <div className="nav__center">
-        <div className="nav__logo">
-        </div>
-    
-        <nav>
-          <ul className="nav__links">
-            <li><Link to={`/`} className="nav__link">Home</Link></li>
-            <li><Link to={`about/`} className="nav__link">About</Link></li>
-            <li><Link to={`tours/`} className="nav__link">Tours</Link></li>
-            <li><Link to={`contact/`} className="nav__link">Contact</Link></li>
-          </ul>
-        </nav>
-      
-        <nav>
-          <ul className="nav__icons">
-            <li><Link to={``} className="nav__icon"><i className="">F</i></Link></li>
-            <li><Link to={``} className="nav__icon"><i className="">Tw</i></Link></li>
-            <li><Link to={``} className="nav__icon"><i className="">Ig</i></Link></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
+    <Header />
     
     <main>
       <div id="page">
