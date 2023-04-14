63c33943d26540bcbdb044a3fa4a3a8d0f860da4
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-04-14 09:47:44 +0200

    Add Link

--- react-libraries/src/components/Hero.jsx
+++ react-libraries/src/components/Hero.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Hero = () => {
   return (
     <section className="hero" id="home">
@@ -7,9 +9,9 @@ const Hero = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
           explicabo debitis est autem dicta.
         </p>
-        <a href="#tours" className="btn hero-btn">
+        <Link to={"tours/"} className="btn hero-btn">
           explore tours
-        </a>
+        </Link>
       </div>
     </section>
   );
