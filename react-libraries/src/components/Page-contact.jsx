519a5bce6a61e02a99e014902a9efdaefa69f6a7
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-04-17 09:13:48 +0200

    Add link to page

--- react-libraries/src/components/Page-contact.jsx
+++ react-libraries/src/components/Page-contact.jsx
@@ -1,68 +1,41 @@
-import { Formik, Form, Field, ErrorMessage } from "formik";
 import Title from "./Title";
+import { tours } from "../data";
 
-export default function ContactForm() {
+const Tours = () => {
   return (
-    <>
-      <Title title="contact" subTitle="form" />
-      <p>Do you have any questions?</p>
+    <section className="section" id="tours">
+      <Title title="featured" subTitle="tours" />
 
-      <Formik
-       initialValues={{ name: '', email: '', message: '' }}
-       validate={values => {
-         const errors = {};
-         if (!values.name) {
-           errors.name = 'Required';
-         } else if (
-           !/^[A-Z]{2,12}$/i.test(values.name)
-         ) {
-           errors.name = 'Please write your name';
-         }
+      <div className="section-center featured-center">
+        {tours.map((tour) => {
+          return (
+            <article className="tour-card">
+              <div className="tour-img-container">
+                <img src={tour.image} className="tour-img" alt="" />
+                <p className="tour-date">{tour.date}</p>
+              </div>
+              <div className="tour-info">
+                <div className="tour-title">
+                  <h4>{tour.title}</h4>
+                </div>
+                <p>{tour.text}</p>
+                <div className="tour-footer">
+                  <p>
+                    <span>
+                      <i className="fas fa-map"></i>
+                    </span>{" "}
+                    {tour.location}
+                  </p>
+                  <p>{tour.duration} days</p>
+                  <p>from ${tour.price}</p>
+                </div>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
-         if (!values.email) {
-           errors.email = 'Required';
-         } else if (
-           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-         ) {
-           errors.email = 'Invalid email address';
-         }
-
-         if (!values.message) {
-           errors.message = 'Required';
-         } else if (
-           !/^[A-Z]{2,50}$/i.test(values.message)
-         ) {
-           errors.message = 'Please write a message';
-         }
-         return errors;
-       }}
-
-       onSubmit={(values, { setSubmitting }) => {
-         setTimeout(() => {
-           alert(JSON.stringify(values, null, 2));
-           setSubmitting(false);
-         }, 400);
-       }}
-     >
-        {({ isSubmitting }) => (
-         <Form>
-            <label htmlFor="name">Name</label>
-            <Field type="text" id="name" name="name" placeholder="Name" />
-            <ErrorMessage name="name" component="div" />
-
-            <label htmlFor="email">Name</label>
-            <Field type="email" id="email" name="email" placeholder="Email" />
-            <ErrorMessage name="email" component="div" />
-
-            <label htmlFor="message">Name</label>
-            <Field type="text" id="message" name="message" as="textarea" placeholder="Message" />
-            <ErrorMessage name="message" component="div" />
-
-            <button type="submit" className="btn" disabled={isSubmitting}>Submit</button>
-         </Form>
-       )}
-
-     </Formik>
-    </>
-  )
-}
+export default Tours;
